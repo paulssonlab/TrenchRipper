9afcf71137f7cb9535289d2f3f6b84961b24545a
Jia <bj66@compute-a-16-163.o2.rc.hms.harvard.edu>
2019-11-17 20:05:50 -0500

    Add padding to allow recognition of POIs on edge of image

--- trenchripper/drift_detection.py
+++ trenchripper/drift_detection.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 import os
 import skimage.io as io
 from sklearn import linear_model
+from skimage.util import pad
 
 def find_seed_image(image_stack, max_poi_std=5):
-  # Find a candidate image in the stack (t x y x x)
+    """
+    
+    """
     num_points = image_stack.shape[0]
     for i in range(num_points-1):
         _, _, std_distance = get_orb_pois(image_stack[i], image_stack[i+1])
@@ -25,22 +28,30 @@ def scale_image(im):
     im_rescaled = im_rescaled.astype(np.uint8)
     return im_rescaled
 
-def get_orb_pois(im1, im2, max_interest_points= 500, match_threshold=0.1):
-    orb = cv2.ORB_create(max_interest_points)
+def get_orb_pois(im1, im2, max_interest_points= 750, match_threshold=0.1, plot=False, pad_width=15):
+    orb = cv2.ORB_create(max_interest_points, patchSize=31)
     im1_scaled = scale_image(im1)
     im2_scaled = scale_image(im2)
+    im1_scaled = pad(im1_scaled, pad_width=pad_width, mode='reflect')
+    im2_scaled = pad(im2_scaled, pad_width=pad_width, mode='reflect')
     matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
     keypoints1, descriptors1 = orb.detectAndCompute(im1_scaled, None)
     keypoints2, descriptors2 = orb.detectAndCompute(im2_scaled, None)
     matches = matcher.match(descriptors1, descriptors2, None)
   # Sort matches by score
     matches.sort(key=lambda x: x.distance, reverse=False)
-
   # Remove not so good matches
     numGoodMatches = int(len(matches) * match_threshold)
     matches = matches[:numGoodMatches]
     distances = [match.distance for match in matches]
     std_distance = np.std(distances)
+    
+    if plot:
+        im3 = cv2.drawMatches(im1_scaled, keypoints1, im2_scaled, keypoints2, matches, None)
+        plt.figure(figsize=(20, 10))
+        plt.imshow(im3)
+        plt.show()
+
     
   # Extract location of good matches
     points1 = np.zeros((len(matches), 2), dtype=np.float32)
@@ -49,6 +60,8 @@ def get_orb_pois(im1, im2, max_interest_points= 500, match_threshold=0.1):
     for i, match in enumerate(matches):
         points1[i, :] = keypoints1[match.queryIdx].pt
         points2[i, :] = keypoints2[match.trainIdx].pt
+    points1 -= pad_width
+    points2 -= pad_width
     return points1, points2, std_distance
 
 def find_drift_poi(points1, points2):
