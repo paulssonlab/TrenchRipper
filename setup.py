61444ad06d56be6623fcde7811ce78b7e91e5783
Eaton <de64@compute-e-16-230.o2.rc.hms.harvard.edu>
2020-04-25 17:08:56 -0400

    bump.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         'parse==1.15.0',
         'qgrid==1.3.1',
         'opencv-python>=4.2.0.34'
-    ]
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DanielScottEaton/TrenchRipper",
