caf57d5065ca4e132409a04ac47fdfaa63c800d0
Eaton <de64@login06.o2.rc.hms.harvard.edu>
2020-04-28 17:58:09 -0400

    Issue with dashboards port forwarding on dask distributed 2.15; reverted requirement to 2.12 until fixed.

--- setup.py
+++ setup.py
@@ -5,19 +5,27 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='TrenchRipper',
-    version='0.1.0',
+    version='0.1.5',
     author="Daniel Eaton",
     packages=setuptools.find_packages(),
     install_requires=[
         'pandas==1.0.3',
         'numpy>=1.18.1',
-        'h5py>=1.10.4',
+        'bokeh==1.4.0',
+        'h5py>=2.10.0',
+        'h5py-cache==1.0',
         'scipy>=1.4.1',
         'scikit-image>=0.16.2',
+        'jupyter-client==6.1.3',
+        'jupyter-core==4.6.3',
         'jupyterlab==2.0.1',
-        'matplotlib>=3.1.3',
-        'dask[complete]==2.12.0',
+        'jupyterlab-server==1.0.7',
+        'jupyter-server-proxy==1.3.2',
+        'matplotlib',
+        'dask==2.12.0',
+        'distributed==2.12.0',
         'dask-jobqueue==0.7.0',
+        'dask-labextension==2.0.1',
         'tifffile>=2020.2.16',
         'ipywidgets==7.5.1',
         'pulp>=1.6.8',
@@ -27,7 +35,6 @@ setuptools.setup(
         'tables>=3.6.1',
         'scikit-learn>=0.22.1',
         'seaborn==0.10.0',
-        'bokeh==1.4.0',
         'h5py_cache==1.0',
         'nd2reader==3.2.3',
         'parse==1.15.0',
@@ -39,3 +46,27 @@ setuptools.setup(
     url="https://github.com/DanielScottEaton/TrenchRipper",
     python_requires='>=3.7',
 )
+
+#BROKEN
+#     - bokeh==2.0.2
+#     - chardet==3.0.4
+#     - click==7.1.2
+#     - cloudpickle==1.4.0
+#     - distributed==2.15.1
+#     - idna==2.9
+#     - pexpect==4.8.0
+#     - pytz==2020.1
+#     - six==1.14.0
+#     - typing-extensions==3.7.4.2
+
+#WORKING
+#     - bokeh==1.4.0
+#     - click==7.1.1
+#     - cloudpickle==1.3.0
+#     - distributed==2.12.0
+#     - importlib-metadata==1.6.0
+#     - pytz==2019.3
+#     - zipp==3.1.0
+
+
+
